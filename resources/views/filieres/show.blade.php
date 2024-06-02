2e6c8fadde4ee42de120b4c7d568fb0c48a70ce4
Rachid AOui <aouirachid22@gmail.com>
2024-06-02 12:48:06 +0100

    add all student in show

--- resources/views/filieres/show.blade.php
+++ resources/views/filieres/show.blade.php
@@ -6,29 +6,26 @@ Consultation  de la filiere : {{$filiere->nom}}
 <ul>
     <Li>Nom : {{$filiere->nom}} </Li>
     <Li>Id : {{$filiere->id}}</Li>
-    {{-- <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Nom</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($etudiants as $etudiant)
-                <tr>
-                    <th scope="row">{{ $etudiant->id }}</th>
-                    <td>{{ $etudiant->nom }}</td>
-                    <td>{{ $etudiant->prenom }}</td>
-                    <td>{{ $etudiant->filiere->nom }}</td>
-                </tr>
-            @endforeach
-
-        </tbody> --}}
-@foreach ($filiere->etudiants as $et)
-<Li>Id : {{$et->nom}}</Li>
-
-@endforeach
 </ul>
+<h1>Liste des etudiant </h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Prenom</th>
+        </tr>
+    </thead>
+    <tbody>
 
+        @foreach ($filiere->etudiants as $et)
+            <tr>
+                <th scope="row">{{ $et->id }}</th>
+                <td>{{ $et->nom }}</td>
+                <td>{{ $et->prenom }}</td>
+            </tr>
+        @endforeach
+
+    </tbody>
+</table>
 @endsection
